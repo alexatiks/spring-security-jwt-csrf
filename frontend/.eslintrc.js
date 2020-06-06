d48657c8ebc7c74bef031b08bb23615f3d5a635d
Alexey Ponomaryov <alexatiks@gmail.com>
2020-06-06 23:04:33 +0300

    eslint configured

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    es6: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
+  ],
+  plugins: [
+    'vue',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 };
